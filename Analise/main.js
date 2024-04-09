eb5b0aafb2cc9234fcb028cc6e658efcf139e729
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2024-04-09 13:53:31 -0300

    Add files via upload

--- Analise/main.js
+++ Analise/main.js
@@ -5,9 +5,10 @@ const {tempoExecucao , analisador_empirico} = require('./TempoExecucao')
 let somaTempos = 0 
 const QTD_REPETICOES = 1;
 for (i=0;i<QTD_REPETICOES;i++){
-    const resultado =analisador_empirico(10000,'Heap')
-    
-    
+    const resultado =analisador_empirico(10**4,'Heap')
+    //const resultado_qud =  analisador_empirico(10,'Quadrático')
+    console.log(resultado.tempo_execucao)
+    //console.log(resultado_qud.tempo_execucao)
 
    
    
