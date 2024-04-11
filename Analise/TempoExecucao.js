235e4d43b30225ea337af351061a93af6b6d692c
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2024-04-11 18:23:56 -0300

    Add files via upload

--- Analise/TempoExecucao.js
+++ Analise/TempoExecucao.js
@@ -16,7 +16,7 @@ function tempoExecucao(funcao){
 
 function analisador_empirico(tamanho_entrada,heap_ou_quadratico){
     const  amostra = gerandoAmostra(tamanho_entrada)
-    console.log("amostrar",amostra)
+    //console.log("amostrar",amostra)
     const objeto_raiz = new OrdenacaoRaizQuadrada(amostra,heap_ou_quadratico,tamanho_entrada)
     var tempo_execucao =  tempoExecucao(()=>objeto_raiz.etapa4())
     const vetor_solucao = objeto_raiz.vetor_solucao
--- Analise/main.js
+++ Analise/main.js
@@ -3,28 +3,22 @@
 //const {OrdenacaoRaizQuadrada,gerandoAmostra} = require('./OrdenacaoRaizQuadrada')
 const {analisador_empirico} = require('./TempoExecucao')
 let somaTempos = 0 
-const QTD_REPETICOES = 2;
-for (let i=0;i<QTD_REPETICOES;i++){
+const QTD_REPETICOES = 9;
+for (let i=1;i<QTD_REPETICOES;i++){
     
-    console.log("Heap")
-    const resultado =analisador_empirico(5,'Heap')
-    console.log("vetor_solucao",resultado.vetor_solucao)
    
-    console.log(resultado.tempo_execucao  ,"valor de i",i )
-    console.log("\n")
-    console.log("Quad")
-    const resultado_qud =  analisador_empirico(5,'Quadrático')
-    console.log("vetor_solucao_quad",resultado_qud.vetor_solucao)
-    console.log("\n")
+    const resultado =analisador_empirico(10**i,'Quadrático')
+    
+   
    
    
     
-
+    console.log("Tempo por i ",i ,resultado.tempo_execucao)
     somaTempos += resultado.tempo_execucao
 
 
 }
 var media = somaTempos/QTD_REPETICOES
 
-console.log("media",media)
+console.log("media Heap",media)
 
